f0da197e88de53602b2d9540e0758ed1bb3bfb88
Rashid <rkhabeer84@gmail.com>
2021-11-09 17:09:39 +0500

    Previous game mode

--- lib/src/scores/api.dart
+++ lib/src/scores/api.dart
@@ -3,18 +3,6 @@ part of kotc_core;
 @RestApi(baseUrl: '')
 abstract class ScoresApi {
   factory ScoresApi() => _ScoresApi(Api.client);
-  //
-  // @GET('/review-mode-scores')
-  // Future<List<ModeScores>> getReviewModeScores();
-  //
-  // @GET('/endless-mode-scores')
-  // Future<List<ModeScores>> getEndlessModeScores();
-  //
-  // @GET('/qbank-mode-scores')
-  // Future<List<ModeScores>> getQBankModeScores();
-  //
-  // @GET('/timed-mode-scores')
-  // Future<List<ModeScores>> getTimedModeScores();
 
   @POST('/scores')
   Future<SimpleResponse> addGameModeScores(@Body() ModeScores score);
@@ -24,6 +12,27 @@ abstract class ScoresApi {
 
   @GET('/scores/high-score/{user}/{type}')
   Future<int> getHighScore(@Path('user') String userId, @Path('type') int type);
+
+  @GET('/scores/history/{user}/{mode}/{last}/{page}')
+  Future<List<PreviousGame>> getPreviousGame(
+    @Path('user') String userId,
+    @Path('mode') int mode,
+    @Path('page') int page,
+    @Path('last') String? last,
+  );
+
+//
+// @GET('/review-mode-scores')
+// Future<List<ModeScores>> getReviewModeScores();
+//
+// @GET('/endless-mode-scores')
+// Future<List<ModeScores>> getEndlessModeScores();
+//
+// @GET('/qbank-mode-scores')
+// Future<List<ModeScores>> getQBankModeScores();
+//
+// @GET('/timed-mode-scores')
+// Future<List<ModeScores>> getTimedModeScores();
 
 // @GET('/timed-mode-scores/high-score/{user}')
 // Future<int> getTimedModeHighScore(@Path('user') String userId);
--- lib/src/scores/response.dart
+++ lib/src/scores/response.dart
@@ -1,6 +1,6 @@
 part of kotc_core;
 
-@JsonSerializable(includeIfNull: false, explicitToJson: false)
+@JsonSerializable(includeIfNull: false, createToJson: false)
 class UserStats {
   String category;
   String categoryId;
@@ -15,4 +15,50 @@ class UserStats {
   });
 
   factory UserStats.fromJson(json) => _$UserStatsFromJson(json);
+}
+
+@JsonSerializable(includeIfNull: false, createToJson: false)
+class PreviousGame {
+  final String id;
+  @JsonKey(name: 'user_id')
+  final String userId;
+  int score;
+  final List<PreviousGameAnswer> answers;
+  int type;
+  final String createdAt;
+
+  PreviousGame({
+    required this.id,
+    required this.type,
+    required this.userId,
+    required this.answers,
+    required this.score,
+    required this.createdAt,
+  });
+
+  factory PreviousGame.fromJson(json) => _$PreviousGameFromJson(json);
+}
+
+@JsonSerializable(includeIfNull: false, createToJson: false)
+class PreviousGameAnswer {
+  final int answer;
+  @JsonKey(name: 'category_id')
+  final String categoryId;
+  @JsonKey(name: 'is_correct')
+  final bool isCorrect;
+  @JsonKey(name: 'question_id')
+  final QuestionResponse questionId;
+  @JsonKey(name: 'subcategory_id')
+  final String subcategoryId;
+
+  PreviousGameAnswer({
+    required this.answer,
+    required this.isCorrect,
+    required this.categoryId,
+    required this.questionId,
+    required this.subcategoryId,
+  });
+
+  factory PreviousGameAnswer.fromJson(json) =>
+      _$PreviousGameAnswerFromJson(json);
 }
