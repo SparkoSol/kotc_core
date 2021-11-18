f24dac7a429fe8025345c4c59ea1dcc5245a9de2
Rashid <rkhabeer84@gmail.com>
2021-11-18 11:49:28 +0500

    Removed extra code

--- lib/src/scores/api.dart
+++ lib/src/scores/api.dart
@@ -20,29 +20,4 @@ abstract class ScoresApi {
     @Path('page') int page,
     @Path('last') String? last,
   );
-
-//
-// @GET('/review-mode-scores')
-// Future<List<ModeScores>> getReviewModeScores();
-//
-// @GET('/endless-mode-scores')
-// Future<List<ModeScores>> getEndlessModeScores();
-//
-// @GET('/qbank-mode-scores')
-// Future<List<ModeScores>> getQBankModeScores();
-//
-// @GET('/timed-mode-scores')
-// Future<List<ModeScores>> getTimedModeScores();
-
-// @GET('/timed-mode-scores/high-score/{user}')
-// Future<int> getTimedModeHighScore(@Path('user') String userId);
-//
-// @GET('/qbank-mode-scores/high-score/{user}')
-// Future<int> getQBankModeHighScore(@Path('user') String userId);
-//
-// @GET('/endless-mode-scores/high-score/{user}')
-// Future<int> getEndlessModeHighScore(@Path('user') String userId);
-//
-// @GET('/review-mode-scores/high-score/{user}')
-// Future<int> getReviewModeHighScore(@Path('user') String userId);
 }
