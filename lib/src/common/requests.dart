fcecf594300df4d9fec10eb0adec6c232fdcfe52
Rashid-khabeer <rkhabeer84@gmail.com>
2022-03-23 17:33:32 +0500

    added version in feedback

--- lib/src/common/requests.dart
+++ lib/src/common/requests.dart
@@ -2,32 +2,35 @@ part of kotc_core;
 
 @JsonSerializable(includeIfNull: false, createFactory: false)
 class ReviewRequest {
-  final String feedback;
-  final String stepsToReproduce;
-  final String email;
-  final String type;
-
   ReviewRequest({
     required this.email,
     required this.type,
     required this.feedback,
+    required this.appVersion,
     required this.stepsToReproduce,
   });
+
+  final String feedback;
+  final String stepsToReproduce;
+  final String email;
+  final String type;
+  @JsonKey(name: 'app_version')
+  final String appVersion;
 
   Map<String, dynamic> toJson() => _$ReviewRequestToJson(this);
 }
 
 @JsonSerializable(includeIfNull: false, createFactory: false)
 class AccountDeletionRequest {
-  final String reason;
-  final String email;
-  final String user;
-
   AccountDeletionRequest({
     required this.email,
     required this.user,
     required this.reason,
   });
+
+  final String reason;
+  final String email;
+  final String user;
 
   Map<String, dynamic> toJson() => _$AccountDeletionRequestToJson(this);
 }
