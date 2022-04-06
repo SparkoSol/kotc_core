46e26ccdf7e83c6f63c3847b4da373298ee39065
Rashid-khabeer <rkhabeer84@gmail.com>
2022-04-06 09:33:05 +0500

    Facts module

---
+++ lib/src/facts/api.dart
@@ -0,0 +1,9 @@
+part of kotc_core;
+
+@RestApi(baseUrl: '')
+abstract class FactsApi {
+  factory FactsApi() => _FactsApi(Api.client);
+
+  @GET('/facts/active')
+  Future<List<FactsResponse>> getFacts();
+}
